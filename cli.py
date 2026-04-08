f1eaac9c6eea2f8c56d4321acc6165822e0d6ed4
Aayushma25 <aayushmamehta@gmail.com>
2026-04-08 20:11:34 +0545

    added the colorama with fallback.

--- cli.py
+++ cli.py
@@ -11,10 +11,15 @@ import time
 from typing import Optional
 
 
-
-
-
-
+try:
+    from colorama import Fore, Style, init as colorama_init
+    colorama_init(autoreset=True)
+    HAS_COLOR = True
+except ImportError:
+    HAS_COLOR = False
+    class _NoColor:
+        def __getattr__(self, _): return ""
+    Fore = Style = _NoColor()
 
 
 
